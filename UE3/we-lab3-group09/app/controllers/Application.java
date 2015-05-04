bc121a933cb5b362f944308a372a7911ece03ef6
Paul P <ppfeiffer93@gmx.at>
2015-05-04 02:39:05 +0200

    Bugs gefixt: Login funktioniert jetzt endlich; Webseiten teilweise angepasst damit sie mit scalar funktionieren

--- UE3/we-lab3-group09/app/controllers/Application.java
+++ UE3/we-lab3-group09/app/controllers/Application.java
@@ -2,87 +2,55 @@ package controllers;
 
 import javax.naming.AuthenticationException;
 
+import model.Login;
 import play.data.*;
 import play.mvc.*;
 import views.html.*;
 
 public class Application extends Controller {
-	
-    public static Result index() {
-    	index.render("test");
-        return redirect( "authentication" );
-    }
-    
-    public static Result login() {
-    	Form<Login> loginForm = Form.form(Login.class).bindFromRequest();
-    	Login loginData = loginForm.get();
-    	System.out.println("Username: " + loginData.getUsername() + " Password: " + loginData.getPassword());
-    	
-    	if(loginForm.get().username.equals("test") && loginForm.get().password.equals("test"))
-    	{
-    		
-    	   System.out.println("Login erfolgreich");
- 	       session().clear();
- 	       session("username", loginForm.get().username);
- 	       return redirect("jeopardy");
-    	}
-    	else
-    	{
-    		System.out.println("Fehler aufgetreten");
-    		return ok(authentication.render());
-    		//return badRequest(authentication.render(loginForm));
-    	}
-    }
-    
 
-    public  static Result auth()
-    {
-    	return ok(authentication.render());
-    	//return ok(authentication.render(Form.form(Login.class)));
-    }
+	public static Result index() {
+		jeopardy2.render("string");
+		return redirect("authentication");
+	}
 
-    public static Result jeopardy() {
-        return ok(jeopardy.render());
-    }
-    
-    public static Result question() {
-        return ok(question.render());
-    }
-    
-    public static Result registration() {
-        return ok(registration.render());
-    }
-    
-    public static Result winner() {
-        return ok(winner.render());
-    }
-    
-    public static  class Login {
-    	public String username;
-    	public String password;
-    	
-    	public String getUsername() {
-    		return username;
-    	}
-    	
-    	public void setUsername(String username) {
-    		this.username = username;
-    	}
-    	public String getPassword() {
-    		return password;
-    	}
-    	public void setPassword(String password) {
-    		this.password = password;
-    	}
-    	
-    	/*
-    	public String validate() {
-    		System.out.println("BLUB " + username + " " + password);
-    	    if (username.equals("test") && password.equals("test")) {
-    	    	return "";
-    	    }
-    	    return "Invalid user or password";
-    	} */
-        
-    }
+	public static Result login() {
+		Form<Login> loginForm = Form.form(Login.class).bindFromRequest();
+
+		try {
+			if (loginForm.hasErrors()) {
+				return badRequest(authentication.render(loginForm));
+			}
+		} catch (Exception e) {
+			return badRequest(authentication.render(loginForm));
+		}
+
+		Login loginData = loginForm.get();
+
+		System.out.println("Login erfolgreich");
+		session().clear();
+		session("username", loginData.username);
+		return redirect("jeopardy");
+
+	}
+
+	public static Result auth() {
+		return ok(authentication.render(Form.form(Login.class)));
+	}
+
+	public static Result jeopardy() {
+		return ok(jeopardy.render());
+	}
+
+	public static Result question() {
+		return ok(question.render());
+	}
+
+	public static Result registration() {
+		return ok(registration.render());
+	}
+
+	public static Result winner() {
+		return ok(winner.render());
+	}
 }
---
+++ UE3/we-lab3-group09/app/model/Login.java
@@ -0,0 +1,29 @@
+package model;
+
+public class Login {
+	public String username;
+	public String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String validate() {
+		if (username.equals("test") && password.equals("test")) {
+			return null;
+		}
+		return "Invalid user or password";
+	}
+}
