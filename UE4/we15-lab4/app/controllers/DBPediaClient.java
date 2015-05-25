ebb56e25f012cc6ac12e6583c9ab393afc2a7363
DjConsti <c.brincoveanu@yahoo.com>
2015-05-25 19:21:00 +0200

    add 5 questions for music

--- UE4/we15-lab4/app/controllers/DBPediaClient.java
+++ UE4/we15-lab4/app/controllers/DBPediaClient.java
@@ -25,112 +25,34 @@ import models.Answer;
 public class DBPediaClient {
 	public static void createDBPediaQuestions() {
 		
-		//Create categories
-		Category category1 = new Category();
-		category1.setName("Filme", "de");
-		category1.setName("Movies", "en");
-		
-		Category category2 = new Category();
-		category2.setName("Musik", "de");
-		category2.setName("Music", "en");
-		
-		Category category3 = new Category();
-		category3.setName("Literatur", "de");
-		category3.setName("Literature", "en");
+		//Create new category
+		Category category = new Category();
+		category.setName("Musik", "de");
+		category.setName("Music", "en");
 
 		//Add questions
-		addMovieQuestion("Welche dieser Filme sind von Tim Burton?",
-				"Which of these movies are from Tim Burton?", 100,
-				"Tim_Burton", "Steven_Spielberg", 2, category1);
-		
 		addMusicQuestion("Welche dieser Lieder sind von den Beatles?",
-				"Which of these songs are from The Beatles?", 100,
-				"The_Beatles", "The_Rolling_Stones", 2, category2);
-		
-		addLiteratureQuestion("Welche dieser Bücher stammen von Ernest Hemingway?",
-				"Which of these novels are written by Ernest Hemingway?", 100,
-				"Ernest_Hemingway", "Mark_Twain", 2, category3);
+				"Which of these songs are from The Beatles?", 50,
+				"The_Beatles", "Creedence_Clearwater_Revival", 3, category);
+		addMusicQuestion("Welche dieser Lieder sind von Eminem?",
+				"Which of these songs are from Eminem?", 40,
+				"Eminem", "50_Cent", 2, category);
+		addMusicQuestion("Welche dieser Lieder sind von den Rolling Stones?",
+				"Which of these songs are from The Rolling Stones?", 30,
+				"The_Rolling_Stones", "The_Beatles", 3, category);
+		addMusicQuestion("Welche dieser Lieder sind von Creedence Clearwater Revival?",
+				"Which of these songs are from Creedence Clearwater Revival?", 20,
+				"Creedence_Clearwater_Revival", "The_Beatles", 4, category);
+		addMusicQuestion("Welche dieser Lieder sind von Daft Punk?",
+				"Which of these songs are from Daft Punk?", 10,
+				"Daft_Punk", "The_Prodigy", 2, category);
 		
 		// Persist categories
-		JeopardyDAO.INSTANCE.persist(category1);
-		JeopardyDAO.INSTANCE.persist(category2);
-		JeopardyDAO.INSTANCE.persist(category3);
+		JeopardyDAO.INSTANCE.persist(category);
+		JeopardyDAO.INSTANCE.persist(category);
+		JeopardyDAO.INSTANCE.persist(category);
 	}
 
-	private static void addMovieQuestion(String deText, String enText, int value,
-			String resourceName, String wrongResourceName, int limit, Category category) {
-		// Create question
-		Question question = new Question();
-		question.setText(deText, "de");
-		question.setText(enText, "en");
-		question.setValue(value);
-
-		if (!DBPediaService.isAvailable())
-			return;
-
-		// Create and execute query
-		Resource resource = DBPediaService.loadStatements(DBPedia
-				.createResource(resourceName));
-		Resource wrongResource = DBPediaService.loadStatements(DBPedia
-				.createResource(wrongResourceName));
-		String englishResourceName = DBPediaService.getResourceName(resource,
-				Locale.ENGLISH);
-		String germanResourceName = DBPediaService.getResourceName(resource,
-				Locale.GERMAN);
-		SelectQueryBuilder itemQuery = DBPediaService.createQueryBuilder()
-				.setLimit(limit).addWhereClause(RDF.type, DBPediaOWL.Film)
-				.addPredicateExistsClause(FOAF.name)
-				.addWhereClause(DBPediaOWL.director, resource)
-				.addFilterClause(RDFS.label, Locale.GERMAN)
-				.addFilterClause(RDFS.label, Locale.ENGLISH);
-		Model resourceItems = DBPediaService.loadStatements(itemQuery
-				.toQueryString());
-		List<String> englishResourceItemNames = DBPediaService
-				.getResourceNames(resourceItems, Locale.ENGLISH);
-		List<String> germanResourceItemNames = DBPediaService
-				.getResourceNames(resourceItems, Locale.GERMAN);
-
-		// Alter and execute query
-		itemQuery.removeWhereClause(DBPediaOWL.director, resource);
-		itemQuery.addMinusClause(DBPediaOWL.director, resource);
-		itemQuery.addWhereClause(DBPediaOWL.director, wrongResource);
-		Model noResourceItems = DBPediaService.loadStatements(itemQuery
-				.toQueryString());
-		List<String> englishNoResourceItemNames = DBPediaService
-				.getResourceNames(noResourceItems, Locale.ENGLISH);
-		List<String> germanNoResourceItemNames = DBPediaService
-				.getResourceNames(noResourceItems, Locale.GERMAN);
-
-		// Add right answers
-		List<Answer> answers = new ArrayList<Answer>();
-		for (int i = 0; i < englishResourceItemNames.size(); i++) {
-			Answer answer = new Answer();
-			answer.setText(englishResourceItemNames.get(i), "en");
-			answer.setText(germanResourceItemNames.get(i), "de");
-			answer.setCorrectAnswer(true);
-			JeopardyDAO.INSTANCE.persist(answer);
-			question.addRightAnswer(answer);
-			answers.add(answer);
-		}
-
-		// Add wrong answers
-		for (int i = 0; i < englishNoResourceItemNames.size(); i++) {
-			Answer answer = new Answer();
-			answer.setText(englishNoResourceItemNames.get(i), "en");
-			answer.setText(germanNoResourceItemNames.get(i), "de");
-			answer.setCorrectAnswer(false);
-			JeopardyDAO.INSTANCE.persist(answer);
-			question.addWrongAnswer(answer);
-			answers.add(answer);
-		}
-
-		// Finish
-		question.setAnswers(answers);
-		JeopardyDAO.INSTANCE.persist(question);
-		question.setCategory(category);
-		category.addQuestion(question);
-	}
-	
 	private static void addMusicQuestion(String deText, String enText, int value,
 			String resourceName, String wrongResourceName, int limit, Category category) {
 		// Create question
@@ -168,80 +90,6 @@ public class DBPediaClient {
 		itemQuery.removeWhereClause(DBPediaOWL.artist, resource);
 		itemQuery.addMinusClause(DBPediaOWL.artist, resource);
 		itemQuery.addWhereClause(DBPediaOWL.artist, wrongResource);
-		Model noResourceItems = DBPediaService.loadStatements(itemQuery
-				.toQueryString());
-		List<String> englishNoResourceItemNames = DBPediaService
-				.getResourceNames(noResourceItems, Locale.ENGLISH);
-		List<String> germanNoResourceItemNames = DBPediaService
-				.getResourceNames(noResourceItems, Locale.GERMAN);
-
-		// Add right answers
-		List<Answer> answers = new ArrayList<Answer>();
-		for (int i = 0; i < englishResourceItemNames.size(); i++) {
-			Answer answer = new Answer();
-			answer.setText(englishResourceItemNames.get(i), "en");
-			answer.setText(germanResourceItemNames.get(i), "de");
-			answer.setCorrectAnswer(true);
-			JeopardyDAO.INSTANCE.persist(answer);
-			question.addRightAnswer(answer);
-			answers.add(answer);
-		}
-
-		// Add wrong answers
-		for (int i = 0; i < englishNoResourceItemNames.size(); i++) {
-			Answer answer = new Answer();
-			answer.setText(englishNoResourceItemNames.get(i), "en");
-			answer.setText(germanNoResourceItemNames.get(i), "de");
-			answer.setCorrectAnswer(false);
-			JeopardyDAO.INSTANCE.persist(answer);
-			question.addWrongAnswer(answer);
-			answers.add(answer);
-		}
-
-		// Finish
-		question.setAnswers(answers);
-		JeopardyDAO.INSTANCE.persist(question);
-		question.setCategory(category);
-		category.addQuestion(question);
-	}
-	
-	private static void addLiteratureQuestion(String deText, String enText, int value,
-			String resourceName, String wrongResourceName, int limit, Category category) {
-		// Create question
-		Question question = new Question();
-		question.setText(deText, "de");
-		question.setText(enText, "en");
-		question.setValue(value);
-
-		if (!DBPediaService.isAvailable())
-			return;
-
-		// Create and execute query
-		Resource resource = DBPediaService.loadStatements(DBPedia
-				.createResource(resourceName));
-		Resource wrongResource = DBPediaService.loadStatements(DBPedia
-				.createResource(wrongResourceName));
-		String englishResourceName = DBPediaService.getResourceName(resource,
-				Locale.ENGLISH);
-		String germanResourceName = DBPediaService.getResourceName(resource,
-				Locale.GERMAN);
-		SelectQueryBuilder itemQuery = DBPediaService.createQueryBuilder()
-				.setLimit(limit).addWhereClause(RDF.type, DBPediaOWL.WrittenWork)
-				.addPredicateExistsClause(FOAF.name)
-				.addWhereClause(DBPediaOWL.author, resource)
-				.addFilterClause(RDFS.label, Locale.GERMAN)
-				.addFilterClause(RDFS.label, Locale.ENGLISH);
-		Model resourceItems = DBPediaService.loadStatements(itemQuery
-				.toQueryString());
-		List<String> englishResourceItemNames = DBPediaService
-				.getResourceNames(resourceItems, Locale.ENGLISH);
-		List<String> germanResourceItemNames = DBPediaService
-				.getResourceNames(resourceItems, Locale.GERMAN);
-
-		// Alter and execute query
-		itemQuery.removeWhereClause(DBPediaOWL.author, resource);
-		itemQuery.addMinusClause(DBPediaOWL.author, resource);
-		itemQuery.addWhereClause(DBPediaOWL.author, wrongResource);
 		Model noResourceItems = DBPediaService.loadStatements(itemQuery
 				.toQueryString());
 		List<String> englishNoResourceItemNames = DBPediaService
